f15c5b2f5843c7e1d7b64063f83d8d8fecb9ade7
Eduardo Santos <4b4c4t3666@gmail.com>
2025-06-18 20:38:45 -0300

    bruh

--- config/app.php
+++ config/app.php
@@ -160,11 +160,11 @@ return [
         /*
         * Application Service Providers...
         */
-        app\Providers\AppServiceProvider::class,
-        app\Providers\EventServiceProvider::class,
-        app\Providers\RouteServiceProvider::class,
-        app\Providers\AuthServiceProvider::class,
-        app\Providers\MercadoPagoServiceProvider::class,
+        App\Providers\AppServiceProvider::class,
+        App\Providers\EventServiceProvider::class,
+        App\Providers\RouteServiceProvider::class,
+        App\Providers\AuthServiceProvider::class,
+        App\Providers\MercadoPagoServiceProvider::class,
     ],
 
     'aliases' => [
